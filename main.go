8d598160d03e5b8679d3b3463ff49b2c6793c1b2
Louis Taylor <louis@kragniz.eu>
2024-11-24 16:39:00 +0000

    Move running rules to separate package

--- main.go
+++ main.go
@@ -5,9 +5,7 @@ import (
 	"crypto/tls"
 	"fmt"
 	"log/slog"
-	"maps"
 	"os"
-	"reflect"
 	"time"
 
 	"github.com/ClickHouse/clickhouse-go/v2"
@@ -15,15 +13,8 @@ import (
 
 	"github.com/kragniz/clickhouse-alertmanager/alert"
 	"github.com/kragniz/clickhouse-alertmanager/config"
+	"github.com/kragniz/clickhouse-alertmanager/rule"
 )
-
-type ScheduledRule struct {
-	LastRun   time.Time
-	Running   bool
-	Config    config.Rule
-	GroupName string
-	Labels    map[string]string
-}
 
 func Fatal(err error) {
 	slog.Error("Fatal", "error", err)
@@ -51,7 +42,7 @@ func connect() (driver.Conn, error) {
 			},
 		},
 
-		TLS: &tls.Config{InsecureSkipVerify: true},
+		TLS: &tls.Config{},
 
 		Debug: false,
 
@@ -77,125 +68,6 @@ func connect() (driver.Conn, error) {
 	return conn, nil
 }
 
-func query(conn driver.Conn, query string) ([]map[string]string, error) {
-	ctx := context.Background()
-	rows, err := conn.Query(ctx, query)
-	if err != nil {
-		return nil, err
-	}
-
-	defer rows.Close()
-
-	var objects []map[string]any
-
-	for rows.Next() {
-		// FIXME: try and do this reflection stuff outside the loop
-
-		columns := rows.ColumnTypes()
-
-		values := make([]any, len(columns))
-		object := map[string]any{}
-
-		for i, column := range columns {
-			object[column.Name()] = reflect.New(column.ScanType()).Interface()
-			values[i] = object[column.Name()]
-		}
-
-		if err = rows.Scan(values...); err != nil {
-			slog.Error("Scanning rows", "error", err)
-			return nil, err
-		}
-
-		objects = append(objects, object)
-	}
-
-	for _, v := range objects {
-		values := []slog.Attr{}
-		for k, v := range v {
-			concreteValue := reflect.ValueOf(v).Elem()
-			values = append(values, slog.Any(k, concreteValue))
-		}
-		slog.LogAttrs(
-			context.Background(),
-			slog.LevelDebug,
-			"Rule found",
-			values...,
-		)
-	}
-
-	labels := []map[string]string{}
-	for _, v := range objects {
-		alertLabels := map[string]string{}
-		for k, v := range v {
-			var value string
-			// TODO: support more types
-			switch v := reflect.ValueOf(v).Elem().Interface().(type) {
-			case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
-				value = fmt.Sprintf("%d", v)
-			case float32, float64:
-				value = fmt.Sprintf("%f", v)
-			case string:
-				value = v
-			default:
-				slog.Warn("Unsupported type", "type", reflect.TypeOf(v))
-				value = fmt.Sprintf("%+v", v)
-			}
-
-			alertLabels[k] = value
-		}
-		labels = append(labels, alertLabels)
-	}
-
-	return labels, nil
-}
-
-func ScheduledRulesFromConfig(c config.AlertConfig) []ScheduledRule {
-	rules := []ScheduledRule{}
-	for _, group := range c.Groups {
-		for _, rule := range group.Rules {
-			labels := maps.Clone(group.Labels)
-			maps.Copy(labels, rule.Labels)
-			rules = append(rules, ScheduledRule{
-				LastRun:   time.Time{},
-				Running:   false,
-				Config:    rule,
-				Labels:    labels,
-				GroupName: group.Name,
-			})
-		}
-	}
-	return rules
-}
-
-func RunRule(rule *ScheduledRule, conn driver.Conn) ([]alert.ActiveAlert, error) {
-	alerts := []alert.ActiveAlert{}
-
-	rule.Running = true
-
-	queryResult, err := query(conn, rule.Config.Expr)
-	if err != nil {
-		rule.Running = false
-		rule.LastRun = time.Now()
-		return nil, err
-	}
-
-	for _, queryLabels := range queryResult {
-		labels := maps.Clone(rule.Labels)
-		maps.Copy(labels, queryLabels)
-		labels["alertname"] = rule.Config.AlertName
-
-		alerts = append(alerts, alert.ActiveAlert{
-			Labels:      labels,
-			Annotations: rule.Config.Annotations,
-		})
-	}
-
-	rule.Running = false
-	rule.LastRun = time.Now()
-
-	return alerts, nil
-}
-
 func main() {
 	logger := slog.New(
 		slog.NewTextHandler(
@@ -210,15 +82,15 @@ func main() {
 
 	conn, err := connect()
 	if err != nil {
-		panic((err))
+		Fatal(err)
 	}
 
 	config, err := config.ReadAlertConfig("alerts.yaml")
 	if err != nil {
-		panic((err))
+		Fatal(err)
 	}
 
-	scheduledRules := ScheduledRulesFromConfig(*config)
+	scheduledRules := rule.ScheduledRulesFromConfig(*config, conn)
 
 	for {
 		for i, scheduledRule := range scheduledRules {
@@ -229,7 +101,7 @@ func main() {
 						"rule", scheduledRule.Config.AlertName,
 					)
 
-					alerts, err := RunRule(&scheduledRules[i], conn)
+					alerts, err := scheduledRules[i].Run()
 					if err != nil {
 						slog.Info("Error running rule",
 							"group", scheduledRule.GroupName,
---
+++ rule/rule.go
@@ -0,0 +1,138 @@
+package rule
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"maps"
+	"reflect"
+	"regexp"
+	"time"
+
+	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+
+	"github.com/kragniz/clickhouse-alertmanager/alert"
+	"github.com/kragniz/clickhouse-alertmanager/config"
+)
+
+type ScheduledRule struct {
+	LastRun   time.Time
+	Running   bool
+	Config    config.Rule
+	GroupName string
+	Labels    map[string]string
+
+	conn driver.Conn
+}
+
+var nonPrometheusLabelRegex = regexp.MustCompile(`[^a-zA-Z0-9_]+`)
+
+func ScheduledRulesFromConfig(c config.AlertConfig, conn driver.Conn) []ScheduledRule {
+	rules := []ScheduledRule{}
+	for _, group := range c.Groups {
+		for _, rule := range group.Rules {
+			labels := maps.Clone(group.Labels)
+			maps.Copy(labels, rule.Labels)
+			rules = append(rules, ScheduledRule{
+				LastRun:   time.Time{},
+				Running:   false,
+				Config:    rule,
+				Labels:    labels,
+				GroupName: group.Name,
+
+				conn: conn,
+			})
+		}
+	}
+	return rules
+}
+
+func (rule *ScheduledRule) Query() ([]map[string]string, error) {
+	ctx := context.Background()
+	rows, err := rule.conn.Query(ctx, rule.Config.Expr)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var objects []map[string]any
+
+	for rows.Next() {
+		// FIXME: try and do this reflection stuff outside the loop
+
+		columns := rows.ColumnTypes()
+
+		values := make([]any, len(columns))
+		object := map[string]any{}
+
+		for i, column := range columns {
+			object[column.Name()] = reflect.New(column.ScanType()).Interface()
+			values[i] = object[column.Name()]
+		}
+
+		if err = rows.Scan(values...); err != nil {
+			slog.Error("Scanning rows", "error", err)
+			return nil, err
+		}
+
+		objects = append(objects, object)
+	}
+
+	labels := []map[string]string{}
+	for _, v := range objects {
+		alertLabels := map[string]string{}
+		for k, v := range v {
+			var value string
+			// TODO: support more types
+			switch v := reflect.ValueOf(v).Elem().Interface().(type) {
+			case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+				value = fmt.Sprintf("%d", v)
+			case float32, float64:
+				value = fmt.Sprintf("%f", v)
+			case string:
+				value = v
+			default:
+				slog.Warn("Unsupported type", "type", reflect.TypeOf(v))
+				value = fmt.Sprintf("%+v", v)
+			}
+
+			k = nonPrometheusLabelRegex.ReplaceAllString(k, "_")
+
+			alertLabels[k] = value
+		}
+		labels = append(labels, alertLabels)
+		slog.Debug("Query found", "labels", alertLabels)
+	}
+
+	return labels, nil
+}
+
+func (rule *ScheduledRule) Run() ([]alert.ActiveAlert, error) {
+	alerts := []alert.ActiveAlert{}
+
+	rule.Running = true
+
+	queryResult, err := rule.Query()
+	if err != nil {
+		rule.Running = false
+		rule.LastRun = time.Now()
+		return nil, err
+	}
+
+	for _, queryLabels := range queryResult {
+		labels := maps.Clone(rule.Labels)
+		maps.Copy(labels, queryLabels)
+		labels["alertname"] = rule.Config.AlertName
+
+		alerts = append(alerts, alert.ActiveAlert{
+			Labels:      labels,
+			Annotations: rule.Config.Annotations,
+		})
+	}
+
+	rule.Running = false
+	rule.LastRun = time.Now()
+
+	return alerts, nil
+}
